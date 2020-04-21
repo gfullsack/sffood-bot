6fa0bf68ba8a0cd98165ae23b1b3959a8e62948e
gfullsack <gfullsack@gmail.com>
2020-04-20 21:04:32 -0700

    4/20/20 update
    saving my work

--- api_keys.py
+++ api_keys.py
@@ -1,10 +1,10 @@
 # Yelp API Key
-yelp_api_key = "hrN8jkRqmK98cAM_adsRKgoePPsCnGCugW0zSZmL_rBS4HUKz8QXF0zP2O7OPgE_LzIKlF-_rBKkoZg_L4e6ZwUbRQ1IGLzLR8Z2VL24fdKvD6llHNX6um5_m8FIXXYx"
+#yelp_api_key = "hrN8jkRqmK98cAM_adsRKgoePPsCnGCugW0zSZmL_rBS4HUKz8QXF0zP2O7OPgE_LzIKlF-_rBKkoZg_L4e6ZwUbRQ1IGLzLR8Z2VL24fdKvD6llHNX6um5_m8FIXXYx"
 
-# Twitter API Key
-Twitter_access_token = "116900081-h1UeDsKuProW2AdYagntTlOi25C7tdqClWBRdBci"
-Twitter_access_token_secret = "XhoXJIIQzlm9KRkNLD0N0jFVg1RErpobzW841gKcuIQK8"
-
-#Consumer API keys
+# Twitter Consumer API keys
 Twitter_api_key = "4ErSgv6StI6qcUVzFvirhP5OK"
-Twitter_api_secret_key = "v8cMVGDa5A0ib62Q9ivW83YWGo7F1iB8dQsd4AaH6d3mx7Nie9"
+Twitter_CONSUMER_SECRET = "v8cMVGDa5A0ib62Q9ivW83YWGo7F1iB8dQsd4AaH6d3mx7Nie9"
+
+# Twitter Access token & access token secret
+Twitter_ACCESS_TOKEN = "116900081-h1UeDsKuProW2AdYagntTlOi25C7tdqClWBRdBci"
+Twitter_ACCESS_TOKEN_SECRET = "XhoXJIIQzlm9KRkNLD0N0jFVg1RErpobzW841gKcuIQK8"
